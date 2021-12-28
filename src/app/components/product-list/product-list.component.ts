6ae76b93f39d574c04a3e92d82f02d5eec3f4b57
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2021-12-28 22:05:35 +0300

    Added product image

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -8,6 +8,8 @@ import { products } from 'src/app/products';
 })
 export class ProductListComponent implements OnInit {
   products =products;
+  myIGM: string = 'assets/img/phones.jpeg'
+
   share(){
     window.alert('The product has been shared!! Thank you')
   }
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,6 +1,7 @@
 export interface Product {
     id: number;
     name: string;
+    phoneImage: string;
     price: number;
     description: string;
   }
@@ -8,39 +9,46 @@ export interface Product {
   export const products = [
     {
       id: 1,
-      name: 'Sumsang',
-      image: './assets/img/samsung.jpeg',
+      name: 'Sumsang Galaxy ',
+      phoneImage : 'assets/img/samsung.jpeg',
       price: 799,
-      description: 'A large phone with one of the best screens'
+      description: " The Samsung Galaxy Z Fold 3 is the latest smartphone that features a 7.6 inch Dynamic AMOLED 2X,  display with a 120Hz screen refresh rate, 12 MP +12 MP + 12 MP triple rear cameras, a single 10MP selfie camera, and a single 4MP Cover camera. It runs on a Qualcomm Snapdragon 888 5G chipset with 12GB of RAM, Android 11 on top of  One UI 3 software. There’s also a side-mounted fingerprint scanner, 5G LTE connectivity, and 256GB or 512GB of expandable storage"
     },
     {
       id: 2,
       name: 'Tecno Camon',
-      image: './assets/img/tecno-camon.jpeg',
+      phoneImage: 'assets/img/tecno-camon.jpeg',
       price: 699,
-      description: 'A great phone with one of the best cameras'
+      description: 'A great phone with one of the best cameras Tecno Camon 17 Pro has a 6.8-inch touchscreen display. Tecno Camon 17 Pro is run by 2.05GHz Octa-core Mediatek’s Helio G95 processor. Tecno Camon 17 Pro has 8GB RAM and a 256GB internal storage expandable to 256GB using a microSD card. Tecno Camon 17 Pro has a 64MP + 8MP + 2MP + 2MP Quad Rear camera and a 48MP Front camera for selfies. Tecno Camon 17 Pro runs Android 11 and has a 5000 mAh Battery.'
     },
     {
       id: 3,
-      name: 'Xiome New',
-      image: './assets/img/xiome.jpeg',
-      price: 299,
-      description: ''
+      name: 'Xiome poco X3 Pro',
+      phoneImage: 'assets/img/xiome.jpeg',
+      price: 430,
+      description: 'Xiaomi Poco X3 Pro is a mid-range smartphone, with high-end specs. With Xiaomi Poco X3 Pro, you get a powerful processor, with a quad-camera system, and fast charging at an affordable price.'
     },
     {
       id: 4,
       name: 'Itel 4G',
-      image: './assets/img/itel.jpeg',
+      phoneImage: 'assets/img/itel.jpeg',
       price: 899,
       description: 'The fastest mobile internet in the market'
     }
     ,
     {
       id: 5,
-      name: 'Nokia 9',
-      image: './assets/img/nokia.jpeg',
-      price: 599,
-      description: 'The fastest mobile internet in the market'
+      name: 'Nokia 5.3',
+      phoneImage: 'assets/img/nokia.jpeg',
+      price: 179,
+      description: `Nokia 5.3
+                    6GB RAM
+                    64GB ROM
+                    6.55" IPS LCD
+                    13MP Quad Rear Camera
+                    Android 10.0 Pie Octa-core
+                    4000mAh Battery
+                    Dubai Warranty`
     }
   ];
   
